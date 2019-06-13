ea7d0487e486f57fc17642c0575b6603d1ef8905
Wild Rabbit <libbey@conservify.org>
2019-06-13 12:39:06 -0700

    Cleanup, query via https

--- FieldKit/app/services/query-device.js
+++ FieldKit/app/services/query-device.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import protobuf from "protobufjs";
-import https from 'https';
 
 const appRoot = protobuf.Root.fromJSON(require("fk-app-protocol"));
 const WireMessageQuery = appRoot.lookupType("fk_app.WireMessageQuery");
@@ -8,15 +7,10 @@ const WireMessageReply = appRoot.lookupType("fk_app.WireMessageReply");
 const QueryType = appRoot.lookup("fk_app.QueryType");
 const ReplyType = appRoot.lookup("fk_app.ReplyType");
 
-// 18 = QUERY_STATUS
-// 10 = QUERY_FILES
-// const encoded = WireMessageQuery.encodeDelimited(message).finish();
-// encoded = 2,8,18
-
 export default class QueryDevice {
     queryStatus() {
-        const message = WireMessageQuery.create({type:18});
-        return this.deviceQuery("http://localhost:2380", message).then(r => {
+        const message = WireMessageQuery.create({type:QueryType.values.QUERY_STATUS});
+        return this.deviceQuery("https://localhost:2382", message).then(r => {
             console.log("reply", r)
         }, e=> {
             console.log("error", e)
@@ -25,7 +19,7 @@ export default class QueryDevice {
 
     queryCapabilities() {
         const message = WireMessageQuery.create({type: QueryType.values.QUERY_CAPABILITIES});
-        return this.deviceQuery("http://localhost:2380", message).then(r => {
+        return this.deviceQuery("https://localhost:2382", message).then(r => {
             console.log("reply", r)
         }, e=> {
             console.log("error", e)
