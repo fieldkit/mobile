da5b998779ff688720ff32dd350e20333c1b10d8
Jacob Lewallen <jlewallen@gmail.com>
2019-09-10 12:06:36 -0700

    Utilities for converting from underscore to camel case.
    Our database naming convention is different, this makes the javascript land
    names the same as our js convention.

---
+++ FieldKit/app/utilities.js
@@ -0,0 +1,33 @@
+// From https://matthiashager.com/converting-snake-case-to-camel-case-object-keys-with-javascript
+
+const isObject = function (o) {
+    return o === Object(o) && !isArray(o) && typeof o !== 'function';
+};
+const isArray = function (a) {
+    return Array.isArray(a);
+};
+const toCamel = (s) => {
+    return s.replace(/([-_][a-z])/ig, ($1) => {
+        return $1.toUpperCase()
+            .replace('-', '')
+            .replace('_', '');
+    });
+};
+export function keysToCamel(o) {
+    if (isObject(o)) {
+        const n = {};
+
+        Object.keys(o).forEach((k) => {
+            n[toCamel(k)] = keysToCamel(o[k]);
+        });
+
+        return n;
+    }
+    else if (isArray(o)) {
+        return o.map((i) => {
+            return keysToCamel(i);
+        });
+    }
+
+    return o;
+};
