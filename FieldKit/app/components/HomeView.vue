9a10e14b6eaa26fcfebde50f25657053a77ffa7f
Jacob Lewallen <jlewallen@gmail.com>
2019-09-10 19:30:59 -0700

    Remove testing code.

--- FieldKit/app/components/HomeView.vue
+++ FieldKit/app/components/HomeView.vue
@@ -4,8 +4,6 @@
             <Label class="plain m-20 text-center" :text="message" textWrap="true"></Label>
             <Button class="btn btn-primary" :text="_L('viewStations')" @tap="viewStations"></Button>
             <Button class="btn btn-secondary" :text="_L('logOut')" @tap="logout"></Button>
-
-            <ProgressBar></ProgressBar>
         </StackLayout>
     </Page>
 </template>
@@ -13,11 +11,6 @@
 <script>
 import Login from "./LoginView";
 import Stations from "./StationsView";
-import Services from "../services/services";
-import ProgressBar from './ProgressBar';
-
-// TODO: Remove
-import { Observable } from "tns-core-modules/data/observable";
 
 export default {
     data() {
@@ -25,51 +18,8 @@ export default {
             message: _L("authenticated")
         };
     },
-    components: {
-        ProgressBar,
-    },
     methods: {
         onPageLoaded() {
-            console.log("loaded");
-
-            if (false) {
-                try {
-                    Services.StateManager().synchronizeLocalData().catch(err => {
-                        console.log(err);
-                    });
-                }
-                catch (error) {
-                    console.log('error', error);
-                }
-            }
-
-            if (this.$stationMonitor.getStations().length > 0) {
-                try {
-                    Services.StateManager().synchronizeConnectedStations().then(() => {
-                        return Services.StateManager().synchronizeLocalData();
-                    }).catch(err => {
-                        console.log(err);
-                    });
-                }
-                catch (e) {
-                    console.log("error", e);
-                }
-            }
-            else {
-            }
-
-            this.$stationMonitor.on(Observable.propertyChangeEvent, () => {
-                try {
-                    Services.StateManager().synchronizeConnectedStations().then(() => {
-                        return Services.StateManager().synchronizeLocalData();
-                    }).catch(err => {
-                        console.log(err);
-                    });
-                }
-                catch (e) {
-                    console.log("error", e);
-                }
-            });
         },
         logout() {
             this.$portalInterface.logout();
