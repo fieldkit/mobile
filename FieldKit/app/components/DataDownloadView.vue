ba30b36b4cbfe0e7ea2822fadfa3b7304c0f4d12
littlecomputer <libbey@conservify.org>
2019-10-24 12:40:53 -0700

    Scan QR code (tested on Android only)

--- FieldKit/app/components/DataDownloadView.vue
+++ FieldKit/app/components/DataDownloadView.vue
@@ -2,13 +2,26 @@
     <Page class="page" actionBarHidden="true" @loaded="onPageLoaded">
         <GridLayout rows="*,80">
             <ScrollView row="0">
-                <StackLayout>
-                    <ScreenHeader title="Data Sync" :canNavigateBack="false" :canNavigateSettings="false" />
-
+                <FlexboxLayout
+                    class="page p-t-10"
+                    flexDirection="column"
+                    justifyContent="space-between"
+                >
+                    <ScreenHeader
+                        title="Data Sync"
+                        :canNavigateBack="false"
+                        :canNavigateSettings="false"
+                    />
                     <SynchronizePanel :station="station" />
-                </StackLayout>
+                    <StackLayout class="m-t-20">
+                        <Button
+                            class="btn btn-primary"
+                            text="Start scanning"
+                            @tap="doScanWithBackCamera">
+                        </Button>
+                    </StackLayout>
+                </FlexboxLayout>
             </ScrollView>
-
             <StationFooterTabs row="1" :station="station" active="data" />
         </GridLayout>
     </Page>
@@ -16,7 +29,7 @@
 
 <script>
 import routes from "../routes";
-
+import { BarcodeScanner } from "nativescript-barcodescanner";
 import SynchronizePanel from "./SynchronizePanel";
 import StationFooterTabs from "./StationFooterTabs";
 import ScreenHeader from "./ScreenHeader";
@@ -27,6 +40,7 @@ export default {
     },
     components: {
         ScreenHeader,
+        BarcodeScanner,
         SynchronizePanel,
         StationFooterTabs
     },
@@ -34,7 +48,39 @@ export default {
         return {};
     },
     methods: {
-        onPageLoaded(args) {}
+        onPageLoaded(args) {},
+
+        scan(front) {
+            new BarcodeScanner().scan({
+                cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
+                cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
+                showFlipCameraButton: true,   // default false
+                showTorchButton: true,        // iOS only, default false
+                torchOn: false,               // launch with the flashlight on (default false)
+                resultDisplayDuration: 500,   // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
+                beepOnScan: true,             // Play or Suppress beep on scan (default true)
+                openSettingsIfPermissionWasPreviouslyDenied: true, // On iOS you can send the user to the settings app if access was previously denied
+                closeCallback: () => {
+                    // console.log("Scanner closed @ " + new Date().getTime());
+                }
+            }).then(result => {
+                // console.log("--- scanned: " + result.text);
+                // Note that this Promise is never invoked when a 'continuousScanCallback' function is provided
+                setTimeout(() => {
+                    alert({
+                        title: "Scan result",
+                        message: "Format: " + result.format + ",\nValue: " + result.text,
+                        okButtonText: "OK"
+                    });
+                }, 200);
+            }, errorMessage => {
+                // console.log("No scan. " + errorMessage);
+            });
+        },
+
+        doScanWithBackCamera() {
+            this.scan(false);
+        },
     }
 };
 </script>
