8e409a314b8ba875b9bcb89cee40e8e0412e89ae
littlecomputer <libbey@conservify.org>
2020-03-12 10:13:30 -0700

    Allow + in email addresses

--- FieldKit/app/components/LoginView.vue
+++ FieldKit/app/components/LoginView.vue
@@ -257,7 +257,7 @@ export default {
             if (this.noEmail) {
                 return;
             }
-            let emailPattern = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+            let emailPattern = /^([a-zA-Z0-9_+\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
             this.emailNotValid = !emailPattern.test(this.user.email);
         },
 
