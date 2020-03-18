463398ad8f8142eef48e04e36d446cd7d64078c7
Jacob Lewallen <jlewallen@gmail.com>
2020-03-18 10:53:30 -0700

    Add tool to create migrations and some documentation.

---
+++ tools/mkm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASE_NAME=$1
+if [ -z "$BASE_NAME" ]; then
+	echo "mkm.sh title_of_migration"
+	exit 2
+fi
+
+APP=FieldKit
+TIME=`date "+%Y%m%d_%H%M%S"`
+FN=${TIME}_${BASE_NAME}.js
+CLASS_NAME=${BASE_NAME}_${TIME}
+
+cp ${APP}/app/migrations/{new_migration.js.template,${FN}}
+
+sed -i "s/{MIGRATION_NAME}/${CLASS_NAME}/gI" ${APP}/app/migrations/${FN}
+
+rm ${APP}/app/migrations/index.js
+
+for f in ${APP}/app/migrations/2020*.js; do
+	FN=`basename $f`
+	echo "export * from './${FN}';" >>  ${APP}/app/migrations/index.js
+done
