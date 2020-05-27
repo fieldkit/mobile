d52f75318f36b33102dcc00983ee8df871b43d7c
Jacob Lewallen <jlewallen@gmail.com>
2020-05-27 14:13:19 -0700

    Narrow down Observable interface to things we need.

--- FieldKit/app/services/observable.js
+++ FieldKit/app/services/observable.js
@@ -1,29 +1,6 @@
 var _ = require("lodash");
 
 Object.defineProperty(exports, "__esModule", { value: true });
-var _wrappedIndex = 0;
-var WrappedValue = (function() {
-    function WrappedValue(wrapped) {
-        this.wrapped = wrapped;
-    }
-    WrappedValue.unwrap = function(value) {
-        return value instanceof WrappedValue ? value.wrapped : value;
-    };
-    WrappedValue.wrap = function(value) {
-        var w = _wrappedValues[_wrappedIndex++ % 5];
-        w.wrapped = value;
-        return w;
-    };
-    return WrappedValue;
-})();
-exports.WrappedValue = WrappedValue;
-var _wrappedValues = [
-    new WrappedValue(null),
-    new WrappedValue(null),
-    new WrappedValue(null),
-    new WrappedValue(null),
-    new WrappedValue(null),
-];
 
 function debugObservers(observers) {
     return _(observers)
@@ -38,18 +15,6 @@ var Observable = (function() {
     function Observable() {
         this._observers = {};
     }
-    Observable.prototype.get = function(name) {
-        return this[name];
-    };
-    Observable.prototype.set = function(name, value) {
-        var oldValue = this[name];
-        if (this[name] === value) {
-            return;
-        }
-        var newValue = WrappedValue.unwrap(value);
-        this[name] = newValue;
-        this.notifyPropertyChange(name, newValue, oldValue);
-    };
     Observable.prototype.on = function(eventNames, callback, thisArg) {
         this.addEventListener(eventNames, callback, thisArg);
     };
@@ -176,62 +141,4 @@ var Observable = (function() {
     return Observable;
 })();
 exports.Observable = Observable;
-var ObservableFromObject = (function(_super) {
-    __extends(ObservableFromObject, _super);
-    function ObservableFromObject() {
-        var _this = (_super !== null && _super.apply(this, arguments)) || this;
-        _this._map = {};
-        return _this;
-    }
-    ObservableFromObject.prototype.get = function(name) {
-        return this._map[name];
-    };
-    ObservableFromObject.prototype.set = function(name, value) {
-        var currentValue = this._map[name];
-        if (currentValue === value) {
-            return;
-        }
-        var newValue = WrappedValue.unwrap(value);
-        this._map[name] = newValue;
-        this.notifyPropertyChange(name, newValue, currentValue);
-    };
-    return ObservableFromObject;
-})(Observable);
-function defineNewProperty(target, propertyName) {
-    Object.defineProperty(target, propertyName, {
-        get: function() {
-            return target._map[propertyName];
-        },
-        set: function(value) {
-            target.set(propertyName, value);
-        },
-        enumerable: true,
-        configurable: true,
-    });
-}
-function addPropertiesFromObject(observable, source, recursive) {
-    if (recursive === void 0) {
-        recursive = false;
-    }
-    Object.keys(source).forEach(function(prop) {
-        var value = source[prop];
-        if (recursive && !Array.isArray(value) && value && typeof value === "object" && !(value instanceof Observable)) {
-            value = fromObjectRecursive(value);
-        }
-        defineNewProperty(observable, prop);
-        observable.set(prop, value);
-    });
-}
-function fromObject(source) {
-    var observable = new ObservableFromObject();
-    addPropertiesFromObject(observable, source, false);
-    return observable;
-}
-exports.fromObject = fromObject;
-function fromObjectRecursive(source) {
-    var observable = new ObservableFromObject();
-    addPropertiesFromObject(observable, source, true);
-    return observable;
-}
-exports.fromObjectRecursive = fromObjectRecursive;
 //# sourceMappingURL=observable.js.map
