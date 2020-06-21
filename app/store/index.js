bc8e3aa626a0af9ee0862e9226e8fcf088a643a6
Jacob Lewallen <jlewallen@gmail.com>
2020-06-20 17:28:07 -0700

    With this as a ts file importing createLogger was broken. Weird.

--- app/store/index.js
+++ app/store/index.js
@@ -1,10 +1,12 @@
 import Vuex from "vuex";
+import createLogger from "vuex/dist/logger";
 import { nearby } from "./modules/nearby";
 import { stations } from "./modules/stations";
 import { phone } from "./modules/phone";
 
 export default function () {
     return new Vuex.Store({
+        plugins: [createLogger()],
         modules: {
             nearby,
             stations,
