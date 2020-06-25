e9e9cdd5255a32160b4d6ec38f5d7337f30c4159
Jacob Lewallen <jlewallen@gmail.com>
2020-06-24 19:31:14 -0700

    Fix paths in migration creation script.

--- tools/mkm.sh
+++ tools/mkm.sh
@@ -6,25 +6,24 @@ if [ -z "$BASE_NAME" ]; then
 	exit 2
 fi
 
-APP=FieldKit
 TIME=`date "+%Y%m%d_%H%M%S"`
 FN=${TIME}_${BASE_NAME}.js
 CLASS_NAME=${BASE_NAME}_${TIME}
 
-cp ${APP}/app/migrations/{new_migration.js.template,${FN}}
+cp app/migrations/{new_migration.js.template,${FN}}
 
 
 kernelName="$(uname -s)"
 case "${kernelName}" in
     Darwin*)
-		sed -i '' "s/{MIGRATION_NAME}/${CLASS_NAME}/g" ${APP}/app/migrations/${FN};;
+		sed -i '' "s/{MIGRATION_NAME}/${CLASS_NAME}/g" app/migrations/${FN};;
     Linux*)
-		sed -i "s/{MIGRATION_NAME}/${CLASS_NAME}/gI" ${APP}/app/migrations/${FN};;
+		sed -i "s/{MIGRATION_NAME}/${CLASS_NAME}/gI" app/migrations/${FN};;
 esac
 
-rm ${APP}/app/migrations/index.js
+rm app/migrations/index.js
 
-for f in ${APP}/app/migrations/2020*.js; do
+for f in app/migrations/2020*.js; do
 	FN=`basename $f`
-	echo "export * from './${FN}';" >>  ${APP}/app/migrations/index.js
+	echo "export * from './${FN}';" >> app/migrations/index.js
 done
