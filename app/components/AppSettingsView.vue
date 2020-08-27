211f8b98ad2cb60d25c91f66895277234319deda
Jacob Lewallen <jlewallen@gmail.com>
2020-08-27 11:42:36 -0700

    Use new platform header on redesigned app settings.
    My editor also reformatted using prettierjs.

--- app/components/AppSettingsView.vue
+++ app/components/AppSettingsView.vue
@@ -1,62 +1,44 @@
 <template>
-    <Page class="page" actionBarHidden="true">
-        <GridLayout rows="75,*,55">
-            <ScreenHeader row="0" :title="_L('appSettings.title')" :canNavigateBack="false" :canNavigateSettings="false" class="m-t-10 m-r-20 m-l-20"/>
-            <ScrollView row="1" class="m-r-20 m-l-20">
+    <Page>
+        <PlatformHeader :title="_L('appSettings.title')" :canNavigateBack="false" :canNavigateSettings="false" />
+        <GridLayout rows="*,55">
+            <ScrollView row="0" class="m-r-20 m-l-20">
                 <StackLayout>
                     <SettingsItemText
                         :link="'dataSync'"
                         :text="'appSettings.data'"
                         :imageSrc="'~/images/icon_connections.png'"
-                        :cssClass="'top-bordered-item'">
-                    </SettingsItemText>
+                        :cssClass="'top-bordered-item'"
+                    ></SettingsItemText>
                     <SettingsItemText
                         :text="'appSettings.notifications'"
-                        :imageSrc="'~/images/icon_settings_notifications.png'">
-                    </SettingsItemText>
-                    <SettingsItemText
-                        :text="'appSettings.units'"
-                        :imageSrc="'~/images/icons_units.png'">
-                    </SettingsItemText>
-                    <SettingsItemText
-                        :text="'appSettings.permissions'"
-                        :imageSrc="'~/images/icon_permissions.png'">
-                    </SettingsItemText>
+                        :imageSrc="'~/images/icon_settings_notifications.png'"
+                    ></SettingsItemText>
+                    <SettingsItemText :text="'appSettings.units'" :imageSrc="'~/images/icons_units.png'"></SettingsItemText>
+                    <SettingsItemText :text="'appSettings.permissions'" :imageSrc="'~/images/icon_permissions.png'"></SettingsItemText>
                     <SettingsItemText
                         :text="'appSettings.appearance'"
-                        :imageSrc="'~/images/icon_lightmode_settings.png'">
-                    </SettingsItemText>
-                    <SettingsItemText
-                        :text="'appSettings.account'"
-                        :imageSrc="'~/images/icon_account_settings.png'">
-                    </SettingsItemText>
-                    <SettingsItemText
-                        :text="'appSettings.help'"
-                        :imageSrc="'~/images/icon_help_settings.png'">
-                    </SettingsItemText>
-                    <SettingsItemText
-                        :text="'appSettings.legal'"
-                        :imageSrc="'~/images/icon_legal_settings.png'">
-                    </SettingsItemText>
+                        :imageSrc="'~/images/icon_lightmode_settings.png'"
+                    ></SettingsItemText>
+                    <SettingsItemText :text="'appSettings.account'" :imageSrc="'~/images/icon_account_settings.png'"></SettingsItemText>
+                    <SettingsItemText :text="'appSettings.help'" :imageSrc="'~/images/icon_help_settings.png'"></SettingsItemText>
+                    <SettingsItemText :text="'appSettings.legal'" :imageSrc="'~/images/icon_legal_settings.png'"></SettingsItemText>
                 </StackLayout>
             </ScrollView>
-            <!-- footer -->
-            <ScreenFooter row="2" active="settings"/>
+            <ScreenFooter row="1" active="settings" />
         </GridLayout>
     </Page>
 </template>
 <script lang="ts">
-    import Vue from "vue";
+import Vue from "vue";
 
-    import ScreenHeader from "./ScreenHeader.vue";
-    import ScreenFooter from "./ScreenFooter.vue";
-    import SettingsItemText from "./SettingsItemText.vue";
+import SharedComponents from "@/components/shared";
+import SettingsItemText from "./SettingsItemText.vue";
 
-    export default Vue.extend({
-        components: {
-            ScreenHeader,
-            ScreenFooter,
-            SettingsItemText
-        }
-    });
+export default Vue.extend({
+    components: {
+        ...SharedComponents,
+        SettingsItemText,
+    },
+});
 </script>
