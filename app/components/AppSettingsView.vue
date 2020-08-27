87188343384de6f630fdf9efcfb2e1e88af4489b
Andrei Hancu <ac@MacBook-Pro.local>
2020-08-27 15:27:41 +0300

    create SettingsItemText and new design to Settings

--- app/components/AppSettingsView.vue
+++ app/components/AppSettingsView.vue
@@ -1,64 +1,62 @@
 <template>
-    <Page class="page" actionBarHidden="true" @loaded="onPageLoaded">
+    <Page class="page" actionBarHidden="true">
         <GridLayout rows="75,*,55">
-            <ScreenHeader row="0" :title="_L('fieldkitSettings')" :canNavigateBack="false" :canNavigateSettings="false" class="m-t-10" />
-            <ScrollView row="1">
-                <FlexboxLayout flexDirection="column" class="p-t-10">
-                    <StackLayout>
-                        <Button v-if="loggedIn" class="btn btn-secondary" :text="_L('logOut')" @tap="logout"></Button>
-                        <Button v-if="!loggedIn" class="btn btn-primary" :text="_L('logIn')" @tap="goToLogin"></Button>
-                    </StackLayout>
-                    <StackLayout class="m-x-10 m-y-20">
-                        <Label :text="'Build: ' + versions.buildNumber" class="size-16 m-b-10" textWrap="true" />
-                        <Label :text="'Time: ' + versions.buildTime" class="size-16 m-b-10" textWrap="true" />
-                        <Label :text="'Tag: ' + versions.buildTag" class="size-16 m-b-10" textWrap="true" />
-                        <Label :text="'Hash: ' + versions.gitHash" class="size-16 m-b-10" textWrap="true" />
-                    </StackLayout>
-                </FlexboxLayout>
+            <ScreenHeader row="0" :title="_L('appSettings.title')" :canNavigateBack="false" :canNavigateSettings="false" class="m-t-10 m-r-20 m-l-20"/>
+            <ScrollView row="1" class="m-r-20 m-l-20">
+                <StackLayout>
+                    <SettingsItemText
+                        :link="'dataSync'"
+                        :text="'appSettings.data'"
+                        :imageSrc="'~/images/icon_connections.png'"
+                        :cssClass="'top-bordered-item'">
+                    </SettingsItemText>
+                    <SettingsItemText
+                        :text="'appSettings.notifications'"
+                        :imageSrc="'~/images/icon_settings_notifications.png'">
+                    </SettingsItemText>
+                    <SettingsItemText
+                        :text="'appSettings.units'"
+                        :imageSrc="'~/images/icons_units.png'">
+                    </SettingsItemText>
+                    <SettingsItemText
+                        :text="'appSettings.permissions'"
+                        :imageSrc="'~/images/icon_permissions.png'">
+                    </SettingsItemText>
+                    <SettingsItemText
+                        :text="'appSettings.appearance'"
+                        :imageSrc="'~/images/icon_lightmode_settings.png'">
+                    </SettingsItemText>
+                    <SettingsItemText
+                        :text="'appSettings.account'"
+                        :imageSrc="'~/images/icon_account_settings.png'">
+                    </SettingsItemText>
+                    <SettingsItemText
+                        :text="'appSettings.help'"
+                        :imageSrc="'~/images/icon_help_settings.png'">
+                    </SettingsItemText>
+                    <SettingsItemText
+                        :text="'appSettings.legal'"
+                        :imageSrc="'~/images/icon_legal_settings.png'">
+                    </SettingsItemText>
+                </StackLayout>
             </ScrollView>
             <!-- footer -->
-            <ScreenFooter row="2" active="settings" />
+            <ScreenFooter row="2" active="settings"/>
         </GridLayout>
     </Page>
 </template>
 <script lang="ts">
-import Vue from "vue";
+    import Vue from "vue";
 
-import ScreenHeader from "./ScreenHeader.vue";
-import ScreenFooter from "./ScreenFooter.vue";
+    import ScreenHeader from "./ScreenHeader.vue";
+    import ScreenFooter from "./ScreenFooter.vue";
+    import SettingsItemText from "./SettingsItemText.vue";
 
-import routes from "@/routes";
-import { Build } from "@/config";
-import Services from "@/services/services";
-
-export default Vue.extend({
-    data(this: any) {
-        return {
-            loggedIn: Services.PortalInterface().isLoggedIn(),
-            versions: Build,
-        };
-    },
-    components: {
-        ScreenHeader,
-        ScreenFooter,
-    },
-    methods: {
-        onPageLoaded(this: any) {},
-        logout(this: any) {
-            Services.PortalInterface().logout();
-            this.$navigateTo(routes.login, {
-                clearHistory: true,
-                props: {
-                    resetUser: true,
-                },
-            });
-        },
-        goToLogin(this: any) {
-            this.$navigateTo(routes.login);
-        },
-    },
-});
+    export default Vue.extend({
+        components: {
+            ScreenHeader,
+            ScreenFooter,
+            SettingsItemText
+        }
+    });
 </script>
-<style scoped lang="scss">
-@import "~/_app-variables";
-</style>
