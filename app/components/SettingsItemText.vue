86fd3f34e957ec5a56ed2b6521685ba1de394c64
Andrei Hancu <ac@MacBook-Pro.local>
2020-09-07 17:45:55 +0300

    common components

---
+++ app/components/SettingsItemText.vue
@@ -0,0 +1,48 @@
+<template>
+    <GridLayout rows="50" columns="*" class="bottom-bordered-item" :class="cssClass" @tap="goToRoute()">
+        <Label :text="_L(text)" class="size-16 m-5 v-middle" row="0" col="1" verticalAlignment="center"/>
+    </GridLayout>
+</template>
+<script lang="ts">
+    import Vue from "vue";
+    import routes from "@/routes";
+
+    export default Vue.extend({
+        data() {
+            return {};
+        },
+        props: {
+            link: {
+                type: String
+            },
+            text: {
+                type: String
+            },
+            cssClass: {
+                type: String
+            }
+        },
+        methods: {
+            goToRoute() {
+                if (this.link) {
+                    this.$navigateTo(routes.appSettings[this.link], {
+                        clearHistory: true,
+                    });
+                }
+            },
+        },
+    });
+</script>
+<style scoped lang="scss">
+    @import "~/_app-variables";
+
+    .bottom-bordered-item {
+        border-bottom-color: $fk-gray-lighter;
+        border-bottom-width: 1;
+    }
+
+    .top-bordered-item {
+        border-top-color: $fk-gray-lighter;
+        border-top-width: 1;
+    }
+</style>
