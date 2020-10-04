f68545dea12b8e73ce4f7e7d2f6fc0e62a861ba3
Jacob Lewallen <jlewallen@gmail.com>
2020-10-04 14:30:20 -0700

    Fix padding in audio recordings area.

--- app/components/deploy/AudioRecordings.vue
+++ app/components/deploy/AudioRecordings.vue
@@ -1,25 +1,27 @@
 <template>
-    <StackLayout>
-        <GridLayout rows="auto" columns="10*,80*,10*" v-for="r in recordings" :key="r.id" class="link-style recording-box">
-            <Image
-                col="0"
-                width="20"
-                class="small-round"
-                src="~/images/Icon_Play.png"
-                v-if="playing != r"
-                @tap="(ev) => playAudio(ev, r)"
-            />
-            <Image
-                col="0"
-                width="20"
-                class="small-round"
-                src="~/images/Icon_Stop.png"
-                v-if="playing == r"
-                @tap="(ev) => stopPlaying(ev, r)"
-            />
-            <Label col="1" :text="getFileName(r)" :data="getFileName(r)" textWrap="true" @tap="(ev) => toggleAudio(ev, r)" />
-            <Image col="2" width="20" class="small-round" src="~/images/Icon_Delete.png" @tap="(ev) => removeRecording(ev, r)" />
-        </GridLayout>
+    <StackLayout class="recordings">
+        <StackLayout v-for="r in recordings" :key="r.id" class="recording-row">
+            <GridLayout rows="auto" columns="10*,80*,10*" class="link-style recording-box">
+                <Image
+                    col="0"
+                    width="20"
+                    class="small-round"
+                    src="~/images/Icon_Play.png"
+                    v-if="playing != r"
+                    @tap="(ev) => playAudio(ev, r)"
+                />
+                <Image
+                    col="0"
+                    width="20"
+                    class="small-round"
+                    src="~/images/Icon_Stop.png"
+                    v-if="playing == r"
+                    @tap="(ev) => stopPlaying(ev, r)"
+                />
+                <Label col="1" :text="getFileName(r)" :data="getFileName(r)" textWrap="true" @tap="(ev) => toggleAudio(ev, r)" />
+                <Image col="2" width="20" class="small-round" src="~/images/Icon_Delete.png" @tap="(ev) => removeRecording(ev, r)" />
+            </GridLayout>
+        </StackLayout>
     </StackLayout>
 </template>
 <script lang="ts">
@@ -79,14 +81,21 @@ export default Vue.extend({
 <style scoped lang="scss">
 @import "~/_app-variables";
 
+.recording-row {
+    padding-top: 10;
+}
 .recording-box {
     padding: 10;
-    margin: 10;
     border-width: 1;
     border-radius: 4;
     border-color: $fk-gray-lighter;
 }
 .link-style {
     color: $fk-primary-blue;
+}
+.small-round {
+    width: 40;
+    padding: 2;
+    border-radius: 20;
 }
 </style>
--- app/components/deploy/MakeAudioRecording.vue
+++ app/components/deploy/MakeAudioRecording.vue
@@ -1,26 +1,28 @@
 <template>
-    <GridLayout rows="auto" columns="10*,10*,70*,10*" class="recording-box">
-        <Image
-            col="0"
-            width="20"
-            class="small-round"
-            src="~/images/Icon_Record.png"
-            v-if="!recording || recording.paused"
-            @tap="startOrResume()"
-        />
-        <Image
-            col="0"
-            width="20"
-            class="small-round"
-            src="~/images/Icon_Pause.png"
-            v-if="recording && !recording.paused"
-            @tap="pause"
-        ></Image>
-        <Image col="1" width="20" class="small-round" src="~/images/Icon_Stop.png" v-if="recording" @tap="stop"></Image>
-        <Label col="2" :colSpan="recording ? 1 : 2" :text="recording.duration | prettyDuration" textWrap="true" v-if="recording" />
-        <Label col="2" colSpan="2" :text="'Press record to begin.'" textWrap="true" v-if="!recording" />
-        <Image col="3" width="20" class="small-round" src="~/images/Icon_Close_Circle.png" @tap="cancel"></Image>
-    </GridLayout>
+    <StackLayout class="recording-container">
+        <GridLayout rows="auto" columns="10*,10*,70*,10*" class="recording-box">
+            <Image
+                col="0"
+                width="20"
+                class="small-round"
+                src="~/images/Icon_Record.png"
+                v-if="!recording || recording.paused"
+                @tap="startOrResume()"
+            />
+            <Image
+                col="0"
+                width="20"
+                class="small-round"
+                src="~/images/Icon_Pause.png"
+                v-if="recording && !recording.paused"
+                @tap="pause"
+            ></Image>
+            <Image col="1" width="20" class="small-round" src="~/images/Icon_Stop.png" v-if="recording" @tap="stop"></Image>
+            <Label col="2" :colSpan="recording ? 1 : 2" :text="recording.duration | prettyDuration" textWrap="true" v-if="recording" />
+            <Label col="2" colSpan="2" :text="'Press record to begin.'" textWrap="true" v-if="!recording" />
+            <Image col="3" width="20" class="small-round" src="~/images/Icon_Close_Circle.png" @tap="cancel"></Image>
+        </GridLayout>
+    </StackLayout>
 </template>
 
 <script lang="ts">
@@ -83,10 +85,8 @@ export default Vue.extend({
 <style scoped lang="scss">
 @import "~/_app-variables";
 
-.large-text-field {
-    border-width: 1;
-    border-color: white;
-    placeholder-color: $fk-gray-hint;
+.recording-container {
+    padding-top: 10;
 }
 .recording-box {
     padding: 10;
@@ -94,10 +94,9 @@ export default Vue.extend({
     border-radius: 4;
     border-color: $fk-gray-lighter;
 }
-.link-style {
-    color: $fk-primary-blue;
-}
-.lighter {
-    color: $fk-gray-text;
+.small-round {
+    width: 40;
+    padding: 2;
+    border-radius: 20;
 }
 </style>
