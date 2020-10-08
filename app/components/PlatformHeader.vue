98e6dbebde5879203d67969887a3e93566c49b52
Jacob Lewallen <jlewallen@gmail.com>
2020-10-08 16:17:41 -0700

    Base back button visibility on stack size.
    This is to fix certain crash scenarios in iOS and force us to maintain
    the backstack properly.

--- app/components/PlatformHeader.vue
+++ app/components/PlatformHeader.vue
@@ -2,6 +2,7 @@
     <ActionBar backgroundColor="white" flat="true">
         <template v-if="ios">
             <template v-if="canNavigateSettings">
+                <NavigationButton text="Back" @tap="raiseBack" :visibility="haveBackStack ? 'visible' : 'collapse'" />
                 <GridLayout rows="auto,auto" columns="*">
                     <Label row="0" class="title m-t-10 m-b-5 text-center text" :text="title"></Label>
                     <Label row="1" class="text-center subtitle text" :text="subtitle" textWrap="true" :visible="subtitle"></Label>
@@ -9,6 +10,7 @@
                 <ActionItem @tap="onSettings" ios.systemIcon="2" ios.position="right" android.systemIcon="ic_menu_edit" />
             </template>
             <template v-else>
+                <NavigationButton text="Back" @tap="raiseBack" :visibility="haveBackStack ? 'visible' : 'collapse'" />
                 <GridLayout rows="auto,auto" columns="*">
                     <Label row="0" class="title m-t-10 m-b-5 text-center text" :text="title"></Label>
                     <Label row="1" class="text-center subtitle text" :text="subtitle" textWrap="true" :visible="subtitle"></Label>
@@ -17,7 +19,7 @@
         </template>
         <template v-else>
             <GridLayout rows="auto" columns="15*,70*,15*" :class="classes">
-                <StackLayout col="0" class="round-bkgd" @tap="raiseBack" v-if="canNavigateBack">
+                <StackLayout col="0" class="round-bkgd" @tap="raiseBack" v-if="haveBackStack">
                     <Image width="21" src="~/images/Icon_Backarrow.png"></Image>
                 </StackLayout>
                 <GridLayout col="1" rows="auto,auto" columns="*">
@@ -36,7 +38,7 @@
 </template>
 <script lang="ts">
 import Vue from "vue";
-import { isIOS } from "@nativescript/core";
+import { Frame, isIOS } from "@nativescript/core";
 
 export default Vue.extend({
     name: "PlatformHeader",
@@ -70,6 +72,7 @@ export default Vue.extend({
             type: Boolean,
             default: false,
         },
+        // Deprecated
         canNavigateBack: {
             type: Boolean,
             default: true,
@@ -92,18 +95,27 @@ export default Vue.extend({
             if (!this.ios) c.push("android");
             return c.join(" ");
         },
+        haveBackStack(this: any): boolean {
+            const frame = Frame.topmost();
+            console.log("platform-header:backStack", frame.backStack.length);
+            return frame.backStack.length > 0;
+        },
     },
     mounted() {
-        console.log("screen-header:mounted", this.ios, this.canNavigateSettings, this.canNavigateBack);
+        // https://docs.nativescript.org/ui/action-bar
+        console.log("platform-header:mounted", this.ios, this.canNavigateSettings, this.canNavigateBack);
+        // https://docs.nativescript.org/api-reference/classes/_ui_frame_.frame.html
+        const frame = Frame.topmost();
+        console.log("platform-header:backStack", frame.backStack.length);
     },
     methods: {
         raiseBack(this: any, ev) {
-            console.log("ScreenHeader:back");
+            console.log("platform-header:back");
             this.$emit("back");
             this.onBack(ev);
         },
         raiseCancel(this: any, ev) {
-            console.log("ScreenHeader:cancel");
+            console.log("platform-header:cancel");
             this.$emit("cancel");
             this.onCancel(ev);
         },
