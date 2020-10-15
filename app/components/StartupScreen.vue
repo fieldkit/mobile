52c959fe197138743ac5e7f9b2010995628af072
Jacob Lewallen <jlewallen@gmail.com>
2020-10-15 16:07:26 -0700

    Remove this:any from station status box.
    This almost definitely broke something.

--- app/components/StartupScreen.vue
+++ app/components/StartupScreen.vue
@@ -226,13 +226,18 @@ export default class StartupScreen extends Vue {
                     clearHistory: true,
                     props: {},
                 });
+                return this.$navigateTo(routes.dataSync, {
+                    clearHistory: true,
+                    props: {},
+                });
+				*/
                 if (services.Store().getters.stationCalibrations[1]) {
                     // return this.$navigateTo(routes.deploy.start, {
                     // return this.$navigateTo(routes.deploy.notes, {
                     // return this.$navigateTo(routes.deploy.review, {
                     // return this.$navigateTo(routes.stationSettings, {
-                    // return this.$navigateTo(routes.stationDetail, {
-                    return this.$navigateTo(routes.onboarding.recalibrate, {
+                    return this.$navigateTo(routes.stationDetail, {
+                        // return this.$navigateTo(routes.onboarding.recalibrate, {
                         clearHistory: true,
                         props: {
                             stationId: 1,
@@ -241,11 +246,6 @@ export default class StartupScreen extends Vue {
                 } else {
                     console.log("no test station");
                 }
-				*/
-                return this.$navigateTo(routes.dataSync, {
-                    clearHistory: true,
-                    props: {},
-                });
             }
 
             console.log("first navigate");
