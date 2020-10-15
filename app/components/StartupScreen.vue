b42f6bae4982e83c274e5ef06d19da4e87163536
Jacob Lewallen <jlewallen@gmail.com>
2020-10-15 12:25:30 -0700

    This logs too much.

--- app/components/StartupScreen.vue
+++ app/components/StartupScreen.vue
@@ -222,15 +222,10 @@ export default class StartupScreen extends Vue {
                     clearHistory: true,
                     props: {},
                 });
-                return this.$navigateTo(routes.dataSync, {
-                    clearHistory: true,
-                    props: {},
-                });
                 return this.$navigateTo(routes.stations, {
                     clearHistory: true,
                     props: {},
                 });
-				*/
                 if (services.Store().getters.stationCalibrations[1]) {
                     // return this.$navigateTo(routes.deploy.start, {
                     // return this.$navigateTo(routes.deploy.notes, {
@@ -246,6 +241,11 @@ export default class StartupScreen extends Vue {
                 } else {
                     console.log("no test station");
                 }
+				*/
+                return this.$navigateTo(routes.dataSync, {
+                    clearHistory: true,
+                    props: {},
+                });
             }
 
             console.log("first navigate");
