0ca1834f913bdbf5c5f85bacc62ee382b599ddb5
Andrei Hancu <ac@MacBook-Pro.local>
2020-10-26 14:08:12 +0200

    AddModuleView component + translations + route

---
+++ app/components/onboarding/AddModuleView.vue
@@ -0,0 +1,119 @@
+<template>
+    <Page class="page" actionBarHidden="true" @loaded="onPageLoaded" @unloaded="onUnloaded">
+        <GridLayout rows="auto,*,80">
+            <GridLayout row="0" rows="auto,auto" columns="*" class="">
+                <StackLayout row="0" verticalAlignment="middle">
+                    <ConnectionStatusHeader :connected="currentStation.connected"/>
+                    <Label class="m-y-20 title text-center" :text="currentStation.name" textWrap="true"></Label>
+                </StackLayout>
+                <GridLayout row="1" rows="auto, auto" columns="*,*" width="80%" class="m-t-10 m-b-20">
+                    <Image row="0" colSpan="2" class="m-b-10 m-l-15 m-r-15" src="~/images/Icon_incomplete.png"/>
+                    <Label row="1" col="0" horizontalAlignment="left" :text="_L('connect')"/>
+                    <Label row="1" col="1" horizontalAlignment="right" :text="_L('setup')"/>
+                </GridLayout>
+            </GridLayout>
+            <StackLayout row="1">
+                <Label order="2" class="instruction" :text="_L('assembleStep4')" lineHeight="4" textWrap="true"></Label>
+                <GridLayout order="4" rows="*" columns="*">
+                    <Image verticalAlignment="middle" v-if="displayFrame" :src="displayFrame"></Image>
+                </GridLayout>
+            </StackLayout>
+            <StackLayout row="2" class="m-x-10">
+                <Button class="btn btn-primary btn-padded" :text="_L('next')" @tap="goNext"></Button>
+            </StackLayout>
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import routes from "@/routes";
+import AppSettings from "@/wrappers/app-settings";
+import * as animations from "../animations";
+import ConnectionStatusHeader from "~/components/ConnectionStatusHeader.vue";
+import {Station} from "~/store";
+
+export default Vue.extend({
+    props: {
+        stationId: {
+            type: Number,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            title: "",
+            instruction: "",
+            buttonText: "",
+            frameImage: "",
+            displayFrame: null,
+            noImageText: null,
+        };
+    },
+    components: {
+        ConnectionStatusHeader
+    },
+    computed: {
+        currentStation(this: any): Station {
+            return this.$store.getters.stationCalibrations[this.stationId];
+        },
+    },
+    methods: {
+        onPageLoaded(this: any) {
+            console.log
+            this._appSettings = new AppSettings();
+            this.animateFrames();
+
+            if (!this.animateFrameTimer) {
+                this.animateFrameTimer = setInterval(this.animateFrames, 1000);
+            }
+        },
+        onUnloaded(this: any) {
+            this.stopAnimation();
+        },
+        goNext(this: any, ev) {
+            return Promise.all([
+                animations.pressed(ev),
+                this.$navigateTo(routes.onboarding.recalibrate, {
+                    clearHistory: true,
+                    props: {
+                        stationId: this.currentStation.id,
+                    },
+
+                })]);
+        },
+        stopAnimation(this: any) {
+            this.displayFrame = null;
+            clearInterval(this.animateFrameTimer);
+            this.animateFrameTimer = null;
+        },
+        animateFrames(this: any) {
+            this.frameImage =
+                this.frameImage == "TI_4-A.jpg" ? "TI_4-B.jpg" : "TI_4-A.jpg";
+            this.displayFrame = this.frameImage ? "~/images/" + this.frameImage : null;
+        },
+    },
+});
+</script>
+
+<style scoped lang="scss">
+@import "~/_app-variables";
+
+.logo {
+    margin-top: 8%;
+    width: 50%;
+}
+
+.instruction {
+    color: $fk-primary-black;
+    text-align: center;
+    font-size: 16;
+    margin-right: 30;
+    margin-left: 30;
+}
+
+.small {
+    width: 50;
+    margin: 20;
+}
+</style>
