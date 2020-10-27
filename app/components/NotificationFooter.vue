8a47f55392e9bfea04dab4cd4c541c8e1d0da1af
Andrei Hancu <ac@MacBook-Pro.local>
2020-10-27 17:49:17 +0200

    format + remove this arg

--- app/components/NotificationFooter.vue
+++ app/components/NotificationFooter.vue
@@ -1,9 +1,8 @@
 <template>
-    <FlexboxLayout justifyContent="center" alignItems="center" class="notify-footer" @tap="showNotifications"
-                   @loaded="onLoaded">
-        <Label class="size-14 m-t-5 m-b-5" :text="_L('notifications')" horizontalAlignment="right"/>
+    <FlexboxLayout justifyContent="center" alignItems="center" class="notify-footer" @tap="showNotifications" @loaded="onLoaded">
+        <Label class="size-14 m-t-5 m-b-5" :text="_L('notifications')" horizontalAlignment="right" />
         <GridLayout rows="auto" columns="*" horizontalAlignment="left">
-            <Label row="0" col="0" :text="notifications.length" class="notify-num text-center"/>
+            <Label row="0" col="0" :text="notifications.length" class="notify-num text-center" />
         </GridLayout>
     </FlexboxLayout>
 </template>
@@ -20,9 +19,8 @@ export default Vue.extend({
         },
     },
     methods: {
-        onLoaded(this: any) {
-        },
-        showNotifications(this: any) {
+        onLoaded() {},
+        showNotifications() {
             return this.$navigateTo(routes.notifications, {});
         },
     },
