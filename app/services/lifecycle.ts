fe5584c6dcf521c0d1ca1bbfc85a301f0f752e45
Jacob Lewallen <jlewallen@gmail.com>
2020-11-10 15:05:49 -0800

    Avoid some log cycle warnings.

--- app/services/lifecycle.ts
+++ app/services/lifecycle.ts
@@ -10,9 +10,9 @@ import DiscoverStation from "./discover-station";
 export default function (discoverStation: () => DiscoverStation): void {
     Application.on(Application.launchEvent, (args: LaunchEventData) => {
         if (args.android) {
-            console.log("lifecycle: launched android:", args.android);
+            console.log("lifecycle: launched android:"/*, args.android*/);
         } else if (args.ios !== undefined) {
-            console.log("lifecycle: launched ios:", args.ios);
+            console.log("lifecycle: launched ios:"/*, args.ios*/);
         }
 
         return null;
@@ -20,9 +20,9 @@ export default function (discoverStation: () => DiscoverStation): void {
 
     Application.on(Application.suspendEvent, (args: ApplicationEventData) => {
         if (args.android) {
-            console.log("lifecycle: suspend:", args.android);
+            console.log("lifecycle: suspend:");
         } else if (args.ios) {
-            console.log("lifecycle: suspend:", args.ios);
+            console.log("lifecycle: suspend:");
         }
 
         if (discoverStation().started()) {
@@ -35,9 +35,9 @@ export default function (discoverStation: () => DiscoverStation): void {
 
     Application.on(Application.resumeEvent, (args: ApplicationEventData) => {
         if (args.android) {
-            console.log("lifecycle: resume:", args.android);
+            console.log("lifecycle: resume:"/*, args.android*/);
         } else if (args.ios) {
-            console.log("lifecycle: resume:", args.ios);
+            console.log("lifecycle: resume:"/*, args.ios*/);
         }
 
         if (!discoverStation().started()) {
@@ -60,20 +60,20 @@ export default function (discoverStation: () => DiscoverStation): void {
         if (args.android) {
             // eslint-disable-next-line
             if (args.android.isFinishing()) {
-                console.log("lifecycle: exit:", args.android);
+                console.log("lifecycle: exit:"/*, args.android*/);
             } else {
-                console.log("lifecycle: exit:", args.android);
+                console.log("lifecycle: exit:"/*, args.android*/);
             }
         } else if (args.ios) {
-            console.log("lifecycle: exit:", args.ios);
+            console.log("lifecycle: exit:"/*, args.ios*/);
         }
     });
 
     Application.on(Application.lowMemoryEvent, (args: ApplicationEventData) => {
         if (args.android) {
-            console.log("lifecycle: lowMemory:", args.android);
+            console.log("lifecycle: lowMemory:"/*, args.android*/);
         } else if (args.ios) {
-            console.log("lifecycle: lowMemory:", args.ios);
+            console.log("lifecycle: lowMemory:"/*, args.ios*/);
         }
     });
 
