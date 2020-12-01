fcff1708751e8441c797635ccf7f3290e974da4f
Jacob Lewallen <jlewallen@gmail.com>
2020-12-01 08:31:52 -0800

    Use native busy indicator, we can style later.

--- app/components/LargeSpinner.vue
+++ app/components/LargeSpinner.vue
@@ -1,14 +1,12 @@
 <template>
     <GridLayout rows="auto, auto" columns="*">
-        <StackLayout row="0" id="loading-circle-blue"></StackLayout>
-        <StackLayout row="0" id="loading-circle-white"></StackLayout>
+        <ActivityIndicator busy="true" row="0"></ActivityIndicator>
         <Label row="1" class="instruction m-t-20" :text="label" lineHeight="4" textWrap="true"></Label>
     </GridLayout>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-// import { promiseAfter } from "@/utilities";
 
 export default Vue.extend({
     props: {
@@ -23,21 +21,6 @@ export default Vue.extend({
 <style scoped lang="scss">
 @import "~/_app-variables";
 
-#loading-circle-blue,
-#loading-circle-white {
-    width: 75;
-    height: 75;
-    background: $fk-gray-white;
-    border-width: 2;
-    border-radius: 60%;
-}
-#loading-circle-white {
-    border-color: $fk-gray-white;
-    clip-path: circle(100% at 50% 0);
-}
-#loading-circle-blue {
-    border-color: $fk-secondary-blue;
-}
 .instruction {
     color: $fk-primary-black;
     text-align: center;
