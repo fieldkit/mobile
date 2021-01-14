5ead04ff3fcf0888ee20d1db3fe0ff2ff7d60ed3
Jacob Lewallen <jlewallen@gmail.com>
2021-01-14 11:34:15 -0800

    Deleting old screen header/footer components.

--- app/components/shared.ts
+++ app/components/shared.ts
@@ -1,11 +1,7 @@
 import PlatformHeader from "./PlatformHeader.vue";
-import ScreenHeader from "./ScreenHeader.vue";
-import ScreenFooter from "./ScreenFooter.vue";
 import LabeledTextField from "./LabeledTextField.vue";
 
 export default {
     PlatformHeader,
-    ScreenHeader,
-    ScreenFooter,
     LabeledTextField,
 };
