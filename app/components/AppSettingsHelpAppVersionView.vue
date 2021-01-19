a5063ce03a72d03f8cd2b3b90a58a96f0a984069
Jacob Lewallen <jlewallen@gmail.com>
2021-01-19 12:12:28 -0800

    Open gh link, this makes copying the version easier.

--- app/components/AppSettingsHelpAppVersionView.vue
+++ app/components/AppSettingsHelpAppVersionView.vue
@@ -6,8 +6,8 @@
                 <StackLayout verticalAlignment="center" backgroundColor="white" class="m-r-30 top-bordered-item bottom-bordered-item">
                     <Label :text="_L('appSettings.help.version')" class="size-16 m-2 m-t-15" />
                     <Label :text="storeVersion" class="size-12 m-2 m-t-0" />
-                    <Label :text="_L('appSettings.help.gitHash')" class="size-16 m-2 m-t-15" />
-                    <StackLayout orientation="horizontal">
+                    <Label :text="_L('appSettings.help.gitHash')" class="size-16 m-2 m-t-15" @tap="openGit" />
+                    <StackLayout orientation="horizontal" @tap="openGit">
                         <Label :text="versions.gitHash.substring(0, 8)" class="size-12 m-2 m-t-0 hash-prefix" />
                         <Label :text="versions.gitHash.substring(8)" class="size-12 m-2 m-t-0 hash-suffix" />
                     </StackLayout>
@@ -26,12 +26,14 @@
 </template>
 <script lang="ts">
 import Vue from "vue";
+import { Dialogs } from "@nativescript/core";
 import { ActionTypes } from "@/store/actions";
 import SharedComponents from "@/components/shared";
 import SettingsItemSlider from "./SettingsItemSlider.vue";
 import SettingsItemText from "./SettingsItemText.vue";
 import { Build } from "@/config";
 import * as appVersion from "nativescript-appversion";
+import * as utils from "@nativescript/core/utils/utils";
 
 export default Vue.extend({
     data(): {
@@ -63,6 +65,18 @@ export default Vue.extend({
     methods: {
         async saveSettings(): Promise<void> {
             await this.$s.dispatch(ActionTypes.UPDATE_SETTINGS, this.currentSettings);
+        },
+        async openGit(): Promise<void> {
+            return Dialogs.confirm({
+                title: "Open GitHub in browser?",
+                okButtonText: _L("yes"),
+                cancelButtonText: _L("no"),
+            }).then((yesNo) => {
+                if (yesNo) {
+                    console.log(`https://github.com/fieldkit/mobile/commit/${Build.gitHash}`);
+                    utils.openUrl(`https://github.com/fieldkit/mobile/commit/${Build.gitHash}`);
+                }
+            });
         },
     },
 });
