cbe9014d3e8bd117660dc8a9ef960b88a2782267
Jacob Lewallen <jlewallen@gmail.com>
2021-02-18 17:42:49 -0800

    New approach to current reading migration.

--- app/migrations/20210217_180912_rename_current_reading.ts
+++ app/migrations/20210217_180912_rename_current_reading.ts
@@ -3,6 +3,6 @@ import { Database } from "@/wrappers/sqlite";
 
 export class rename_current_reading_20210217_180912 extends Migration {
     public async up(db: Database): Promise<void> {
-        return db.batch(["ALTER TABLE sensors RENAME COLUMN current_reading TO reading"]);
+        return db.batch(["ALTER TABLE sensors ADD COLUMN reading", "UPDATE sensors SET current_reading = reading"]);
     }
 }
