c9a59a5f9669773e9482b7fceb093d320d263873
Jacob Lewallen <jlewallen@gmail.com>
2021-02-18 17:44:44 -0800

    Add type, silly sqlite.

--- app/migrations/20210217_180912_rename_current_reading.ts
+++ app/migrations/20210217_180912_rename_current_reading.ts
@@ -3,6 +3,6 @@ import { Database } from "@/wrappers/sqlite";
 
 export class rename_current_reading_20210217_180912 extends Migration {
     public async up(db: Database): Promise<void> {
-        return db.batch(["ALTER TABLE sensors ADD COLUMN reading", "UPDATE sensors SET reading = current_reading"]);
+        return db.batch(["ALTER TABLE sensors ADD COLUMN reading NUMERIC", "UPDATE sensors SET reading = current_reading"]);
     }
 }
