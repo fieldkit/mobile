ebb380d05eedf406ba6ee8f2c205cae210b53e9a
Jacob Lewallen <jlewallen@gmail.com>
2021-02-22 16:03:59 -0800

    Adding easier to use hide keyboard code.
    Got this from:
    
    https://github.com/NativeScript/NativeScript/issues/4594

--- app/lib/index.ts
+++ app/lib/index.ts
@@ -5,3 +5,14 @@ export * from "./timer";
 export * from "./errors";
 export * from "./testing";
 export * from "./database";
+
+import * as nsutils from "@nativescript/core/utils/utils";
+import { ios } from "@nativescript/core/application";
+
+export function hideKeyboard(): void {
+    if (ios) {
+        ios.nativeApp.sendActionToFromForEvent("resignFirstResponder", null, null, null);
+    } else {
+        nsutils.ad.dismissSoftInput();
+    }
+}
