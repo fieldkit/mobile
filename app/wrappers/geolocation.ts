80aa6371efd22aae89cea037ec3fccb9951323d6
Jacob Lewallen <jlewallen@gmail.com>
2021-06-16 13:18:18 -0700

    chore: Add Apple tracking transparency plugin.

--- app/wrappers/geolocation.ts
+++ app/wrappers/geolocation.ts
@@ -1,4 +1,6 @@
+import { TrackingTransparency, TrackingStatus } from "@nstudio/nativescript-tracking-transparency";
 import { isEnabled, getCurrentLocation, enableLocationRequest, Location } from "@nativescript/geolocation";
+import { debug } from "@/lib";
 
 export interface QueryLocationParams {
     desiredAccuracy: number;
@@ -8,8 +10,30 @@ export interface QueryLocationParams {
 }
 
 export class GeoLocation {
-    public isEnabled(): Promise<boolean> {
-        return isEnabled();
+    private async getTrackingTransparencyStatus(): Promise<TrackingStatus> {
+        const status = TrackingTransparency.getTrackingStatus();
+        debug.log(`tracking-transparency-status: ${status}`);
+        if (status == TrackingStatus.NotDetermined) {
+            return await TrackingTransparency.requestTrackingPermission();
+        }
+        return status;
+    }
+
+    private async canTrack(): Promise<boolean> {
+        const status = await this.getTrackingTransparencyStatus();
+        debug.log(`tracking-transparency-status: ${status}`);
+        return status === TrackingStatus.Authorized || status === TrackingStatus.Unavailable;
+    }
+
+    public async isEnabled(enableTrackingTransparency = false): Promise<boolean> {
+        if (!enableTrackingTransparency) {
+            return isEnabled();
+        }
+        const canEnable = await this.canTrack();
+        if (canEnable) {
+            return isEnabled();
+        }
+        return false;
     }
 
     public getCurrentLocation(params: QueryLocationParams): Promise<Location> {
