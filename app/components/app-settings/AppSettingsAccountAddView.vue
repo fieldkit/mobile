f66969c8aa11b1b95df083173648f1bb079fbf4c
Andrei Radoi <andrei.radoi@neurony.ro>
2021-09-03 01:05:22 +0300

    add a internet needed message in login/register view

--- app/components/app-settings/AppSettingsAccountAddView.vue
+++ app/components/app-settings/AppSettingsAccountAddView.vue
@@ -2,6 +2,10 @@
     <Page>
         <PlatformHeader :title="_L('appSettings.account.addAccount')" :canNavigateSettings="false" :canCancel="true" />
         <SettingsLayout class="m-x-10">
+            <StackLayout v-if="!isOnline" orientation="horizontal" class="banner-internet">
+                <Image width="20" class="banner-internet-img" src="~/images/Icon_Warning_error.png"></Image>
+                <Label :text="_L('mustBeConnected')" />
+            </StackLayout>
             <GridLayout rows="*,auto">
                 <LoginForm row="0" v-if="login" :allowContinueOffline="false" :busy="busy" @saved="onLoginSaved" />
 
@@ -26,6 +30,7 @@ import SharedComponents from "@/components/shared";
 import { Dialogs } from "@nativescript/core";
 import { debug } from "@/lib";
 import { fullRoutes } from "~/routes";
+import axios from "axios";
 
 export default Vue.extend({
     name: "AppSettingsAccountAddView",
@@ -37,11 +42,16 @@ export default Vue.extend({
     data(): {
         login: boolean;
         busy: boolean;
+        isOnline: boolean;
     } {
         return {
             login: true,
             busy: false,
+            isOnline: true,
         };
+    },
+    mounted() {
+        this.checkIfOnline();
     },
     methods: {
         toggle(): void {
@@ -70,6 +80,10 @@ export default Vue.extend({
                 // this.busy = false;
             }
         },
+        async checkIfOnline() {
+            const response = await axios.request({ url: "https://google.com", timeout: 3000 });
+            this.isOnline = response.status === 200;
+        },
     },
 });
 </script>
@@ -87,5 +101,15 @@ export default Vue.extend({
     font-size: 14;
     margin-bottom: 10;
     font-weight: bold;
+}
+
+.banner-internet {
+    background: $fk-gray-lightest;
+    padding: 10 15;
+    font-size: 14;
+
+    &-img {
+        margin-right: 12;
+    }
 }
 </style>
